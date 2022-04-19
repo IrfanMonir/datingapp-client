042524025a0e18d1c608504962e4287ed5dfedd3
IrfanMonir <uddin@s3innovate.com>
2022-04-19 15:10:55 +0600

    added bootstrap and fon-awesom and fetch data from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'datingapp-client';
+  users: any;
+
+  constructor(private http:HttpClient){
+
+  }
+  ngOnInit(): void {
+   this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5258/api/users').subscribe(response => {
+      this.users = response;
+      console.log(this.users);
+    }, error => {
+      console.log(error);
+    })
+  }
 }
